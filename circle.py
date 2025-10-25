22ef5702760d18274b7c9de016e224e2c496b35b
Игорь Романов <igorromanov@MacBook-Air-Igor-2.local>
2025-10-25 18:40:33 +0300

    documentashion

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает радиус круга и возращает его площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус круга и возращает его периметр'''
     return 2 * math.pi * r
 
